fb5dfba119c7e823a99c383d2315a13f4237dc35
Nazlı Sunay Bulbul <nazlisunaybulbul@gmail.com>
2024-10-21 18:51:26 +0300

    AppDb Eklendi

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using IdmhProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdmhProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categoryies { get; set; }
+        public DbSet<ContactFormSubmission> ContactFormSubmissions { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectTeamMember> ProjectTeamMembers { get; set; } // Kavşak Tablosu
+        public DbSet<StaticContent> StaticContents {  get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Many-to-Many Relationship between Projects and TeamMembers
+            modelBuilder.Entity<ProjectTeamMember>()
+                .HasKey(pt => new { pt.ProjectId, pt.TeamMemberId });
+
+            modelBuilder.Entity<ProjectTeamMember>()
+                .HasOne(pt => pt.Project)
+                .WithMany(p => p.ProjectTeamMembers)
+                .HasForeignKey(pt => pt.ProjectId);
+
+            modelBuilder.Entity<ProjectTeamMember>()
+                .HasOne(pt => pt.TeamMember)
+                .WithMany(t => t.ProjectTeamMembers)
+                .HasForeignKey(pt => pt.TeamMemberId);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using IdmhProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace IdmhProject
 {
     public class Program
@@ -8,6 +11,11 @@ namespace IdmhProject
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
