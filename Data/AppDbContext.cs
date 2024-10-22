f55831441e33c66b43f3f428827f668922b1e274
neslihannur <hanneslihan34@gmail.com>
2024-10-23 02:39:53 +0300

    ParentCategory eklendi. AppDbContext te değişiklik yapıldı.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,25 +8,27 @@ namespace IdmhProject.Data
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
-
+        public DbSet<ParentCategory> ParentCategories { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ContactFormSubmission> ContactFormSubmissions { get; set; }
         public DbSet<Project> Projects { get; set; }
-        
-        public DbSet<StaticContent> StaticContents {  get; set; }
+
+        public DbSet<StaticContent> StaticContents { get; set; }
         public DbSet<TeamMember> TeamMembers { get; set; }
-
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Project>()
-                .HasOne(p => p.Category) // "Categories" yerine "Category" yazdık
-                .WithMany(c => c.Projects) // Category'nin birden fazla projeye sahip olduğunu belirtir
-                .HasForeignKey(p => p.CategoryId); // Yabancı anahtar
-        }
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Projects)
+                .HasForeignKey(p => p.CategoryId);
 
+            modelBuilder.Entity<Category>()
+                .HasOne(c => c.ParentCategory) // Category'nin ParentCategory ile ilişkisi
+                .WithMany(pc => pc.SubCategories) // ParentCategory'nin birden fazla alt kategoriye sahip olduğunu belirtir
+                .HasForeignKey(c => c.ParentCategoryId); // Yabancı anahtar
+        }
     }
 }
---
+++ Migrations/20241022233821_ParentCategory.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdmhProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParentCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCategoryId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ParentCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParentCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_ParentCategories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId",
+                principalTable: "ParentCategories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_ParentCategories_ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "ParentCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCategoryId",
+                table: "Categories");
+        }
+    }
+}
