c6fc503d8d303bf9b8ffcff91fe8e996d3ec224c
neslihannur <hanneslihan34@gmail.com>
2024-10-22 20:44:19 +0300

    Home değiştirildi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,50 @@
+using IdmhProject.Data;
 using IdmhProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace IdmhProject.Controllers
 {
     public class HomeController : Controller
     {
-        
+            private readonly AppDbContext _context;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-         public IActionResult Architecture()
-        {
-            return View();
-        }
-        public IActionResult InteriorArchitecture()
-        {
-            return View();
-        }
+            public HomeController(AppDbContext context)
+            {
+                _context = context;
+            }
 
 
-        public IActionResult Privacy()
-        {
-            return View();
+            public IActionResult Index()
+            {
+                return View();
+            }
+            public IActionResult Architecture()
+            {
+                var projects = _context.Projects
+                    .Include(p => p.Categories)
+                    .Where(p => p.CategoryId == 1) // Mimarlýk kategorisi
+                    .ToList();
+
+                return View(projects);
+            }
+
+            public IActionResult InteriorArchitecture()
+            {
+                var projects = _context.Projects
+                    .Include(p => p.Categories)
+                    .Where(p => p.CategoryId == 2) // Ýç Mimarlýk kategorisi
+                    .ToList();
+
+                return View(projects);
+            }
+
+
+
+            public IActionResult Privacy()
+            {
+                return View();
+            }
         }
     }
-}
